8f3ac9ada990b1f81d798336b8305b010d222181
Zichun Ye <zichun.ye@huawei.com>
2025-05-15 10:08:39 +0800

    update patch

--- src/op_plugin.cpp
+++ src/op_plugin.cpp
@@ -34,7 +34,7 @@ extern "C" int ZerosLikeExt(int nparam, void **params, int *ndims, int64_t **sha
                             void *extra) {
   auto tensors = get_torch_tensors(nparam, params, ndims, shapes, dtypes, c10::kCPU);
   auto at_input = tensors[0];
-  auto at_output = tensors[1];
+  auto at_output = tensors[2];
   at::zeros_like_out(at_output, at_input);
   return 0;
 }
@@ -48,7 +48,7 @@ extern "C" int StackExt(int nparam, void **params, int *ndims, int64_t **shapes,
   tensors.resize(nparam - 2);
 
   KernelInputInfo *kernel_input_info = static_cast<KernelInputInfo *>(extra_void);
-  int64_t dim = kernel_input_info->GetKernelInput<int64_t>(2);
+  int64_t dim = kernel_input_info->GetKernelInput<int64_t>(nparam - 2);
 
   at::stack_out(at_output, tensors, dim);
   return 0;
