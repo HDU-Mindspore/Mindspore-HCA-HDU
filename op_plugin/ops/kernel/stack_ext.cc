b1a1d071d34d8f3e191224da46dc2795d8a61e1f
Zichun Ye <zichun.ye@huawei.com>
2025-05-22 11:42:50 +0800

    fix stack; add stack bprop op

--- op_plugin/ops/kernel/stack_ext.cc
+++ op_plugin/ops/kernel/stack_ext.cc
@@ -13,8 +13,23 @@ extern "C" int StackExt(int nparam, void **params, int *ndims, int64_t **shapes,
   tensors.resize(nparam - 2);
 
   KernelInputInfo *kernel_input_info = static_cast<KernelInputInfo *>(extra_void);
-  int64_t dim = kernel_input_info->GetKernelInput<int64_t>(2);
+  int64_t dim = kernel_input_info->GetKernelInput<int64_t>(nparam - 2);
 
   at::stack_out(at_output, tensors, dim);
+  return 0;
+}
+
+extern "C" int UnstackExtView(int nparam, void **params, int *ndims, int64_t **shapes, const char **dtypes,
+                              void *stream, void *extra_void){
+  auto tensors = get_torch_tensors(nparam, params, ndims, shapes, dtypes, c10::kCPU);
+
+  auto input_tensor = tensors[0];
+
+  tensors.erase(tensors.begin(), tensors.begin() + 2);
+
+  KernelInputInfo *kernel_input_info = static_cast<KernelInputInfo *>(extra_void);
+  int64_t dim = kernel_input_info->GetKernelInput<int64_t>(1);
+
+  at::stack_out(tensors, input_tensor, dim);
   return 0;
 }
