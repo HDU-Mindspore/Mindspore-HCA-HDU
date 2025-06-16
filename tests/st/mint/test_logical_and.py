08e994d05bc70fcba50e190dd207412f58922899
baochong <zjbc123@sina.com>
2025-06-16 15:24:39 +0800

    add cpu logical_and logical_not

---
+++ tests/st/mint/test_logical_and.py
@@ -0,0 +1,81 @@
+# Copyright 2025 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+# pylint: disable=unused-variable
+import pytest
+import numpy as np
+import mindspore as ms
+from mindspore import ops, mint, jit
+from tests.utils.test_op_utils import TEST_OP
+from tests.utils.mark_utils import arg_mark
+import torch
+
+
+def generate_random_input(shape, dtype):
+    return np.random.uniform(-1, 1, shape).astype(dtype)
+
+
+def generate_ones_grad(shape, dtype):
+    return np.ones(shape).astype(dtype)
+
+
+def generate_expect_forward_output(x, other, rounding_mode=None):
+    return torch.logical_and(x, other, rounding_mode)
+
+
+def generate_expect_backward_output(x, other, grad, rounding_mode=None):
+    x.requires_grad = True
+    out = torch.logical_and(x, other, rounding_mode)
+    out.backward(grad)
+    dx = x.grad
+    return dx
+
+
+def logical_and_forward_func(x, other, rounding_mode=None):
+    return mint.logical_and(x, other, rounding_mode)
+
+
+def logical_and_backward_func(x, other, grad, rounding_mode=None):
+    return ops.grad(logical_and_forward_func, (0,))(x, other)
+
+
+@arg_mark(plat_marks=['cpu_linux'], level_mark='level0', card_mark='onecard', essential_mark='essential')
+@pytest.mark.parametrize('mode', ['pynative'])
+def test_logical_and_std(mode):
+    """
+    Feature: standard forward, backward features.
+    Description: test function logical_and.
+    Expectation: expect correct result.
+    """
+    x = generate_random_input((2, 3, 4), np.float32)
+    other = generate_random_input((2, 3, 4), np.float32)
+    expect = generate_expect_forward_output(torch.Tensor(x), torch.Tensor(other))
+
+    grad = generate_ones_grad(expect.shape, expect.numpy().dtype)
+    expect_grad = generate_expect_backward_output(torch.Tensor(x), torch.Tensor(other), torch.Tensor(grad))
+
+    ms_x = ms.Tensor(x)
+    ms_other = ms.Tensor(other)
+    if mode == 'pynative':
+        ms.context.set_context(mode=ms.PYNATIVE_MODE)
+        output = logical_and_forward_func(ms_x, ms_other)
+        output_grad = logical_and_backward_func(ms_x, ms_other)
+    else:
+        output = (jit(logical_and_forward_func, backend="ms_backend", jit_level="O0"))(ms_x, ms_other)
+        output_grad = (jit(logical_and_backward_func, backend="ms_backend", jit_level="O0"))(ms_x, ms_other)
+
+    assert np.allclose(output.asnumpy(), expect.detach().numpy(), equal_nan=True)
+    assert np.allclose(output_grad.asnumpy(), expect_grad.detach().numpy(), equal_nan=True)
+
+
